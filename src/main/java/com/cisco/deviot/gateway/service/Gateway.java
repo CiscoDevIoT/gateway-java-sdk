5ebac7854513b88cb4ed0df4b015ee76959b16d7
Hai-Hua Xiao <haihxiao@cisco.com>
2017-02-05 20:28:56 +0800

    reformat

--- src/main/java/com/cisco/deviot/gateway/service/Gateway.java
+++ src/main/java/com/cisco/deviot/gateway/service/Gateway.java
@@ -17,7 +17,7 @@ public class Gateway {
 	private final Logger log = LoggerFactory.getLogger(Gateway.class);
 
 	private static final String API_URL = "/api/v1/gateways";
-	
+
 	private GatewayModel gateway;
 	private String account;
 	private String deviotServer;
@@ -38,17 +38,17 @@ public class Gateway {
 	public void sendData() {
 		this.connector.publish(getData());
 	}
-	
+
 	public void sendData(Map<String, Object> data) {
 		this.connector.publish(data);
 	}
-	
+
 	public Map<String, Object> getData() {
 		return this.gateway.getData();
 	}
 
 	public Gateway register(String id, String name, Object thing) {
-		if(gateway.containsThing(id)) {
+		if (gateway.containsThing(id)) {
 			log.warn("thing {} already registered", id);
 		} else {
 			this.gateway.addThing(id, name, thing);
@@ -56,9 +56,9 @@ public class Gateway {
 		}
 		return this;
 	}
-	
+
 	public Gateway unregister(String id) {
-		if(gateway.containsThing(id)) {
+		if (gateway.containsThing(id)) {
 			log.info("thing {} unregistered", id);
 			gateway.removeThing(id);
 		} else {
@@ -66,9 +66,9 @@ public class Gateway {
 		}
 		return this;
 	}
-	
+
 	public void start() {
-		if(started) {
+		if (started) {
 			log.warn("Gateway servie {} started already", gateway.name);
 		} else {
 			started = true;
@@ -82,25 +82,28 @@ public class Gateway {
 			log.info("Gateway servie {} started", gateway.name);
 		}
 	}
-	
+
 	private void registerSelf() {
 		Boolean failed = null;
 		Map<String, Object> gatewayModel = new HashMap<>();
 		gatewayModel.put("name", gateway.name);
 		gatewayModel.put("mode", ServiceMode.MQTT.ordinal());
-    	gatewayModel.put("owner", account);
-    	gatewayModel.put("host", connector.getHost());
-    	gatewayModel.put("port", connector.getPort());
-    	gatewayModel.put("data", connector.getData());
-    	gatewayModel.put("action", connector.getAction());
-    	gatewayModel.put("sensors", gateway.getModels());
-		while(started) {
-	    	try {
+		gatewayModel.put("owner", account);
+		gatewayModel.put("host", connector.getHost());
+		gatewayModel.put("port", connector.getPort());
+		gatewayModel.put("data", connector.getData());
+		gatewayModel.put("action", connector.getAction());
+		gatewayModel.put("sensors", gateway.getModels());
+		while (started) {
+			try {
 				HttpUtils.postJson(deviotServer + API_URL, gatewayModel, Collections.emptyMap());
-				if((failed == null || failed)) log.info("Gateway service {} registered to {}", gateway.name, deviotServer);
+				if ((failed == null || failed))
+					log.info("Gateway service {} registered to {}", gateway.name, deviotServer);
 				failed = false;
 			} catch (IOException e) {
-				if(failed == null || !failed) log.error("Gateway service {} registered to {} failed: {}", gateway.name, deviotServer, e.getMessage());
+				if (failed == null || !failed)
+					log.error("Gateway service {} registered to {} failed: {}", gateway.name, deviotServer,
+							e.getMessage());
 				failed = true;
 			}
 			try {
@@ -111,9 +114,9 @@ public class Gateway {
 		}
 		started = false;
 	}
-	
+
 	public void stop() {
-		if(started) {
+		if (started) {
 			try {
 				HttpUtils.delete(deviotServer + API_URL + "/" + gateway.name, Collections.emptyMap());
 				log.info("Gateway service {} deregistered from {}", gateway.name, deviotServer);
